f4f62d30240b49cf98732220450e64f2290727cc
phongit03 <138546809+phongit03@users.noreply.github.com>
2023-12-18 17:48:18 +0700

    Added message service, component; added routing module

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 
 @Component({
@@ -9,11 +11,26 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent {
-  heroes = HEROES;
-  hero? : Hero;
+export class HeroesComponent implements OnInit{
 
-  onSelect(hero: Hero) {
-    this.hero = hero;
+  constructor(private heroService: HeroService, private messageService: MessageService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  heroes: Hero[] = [];
+  
+ 
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(res=>{
+      this.heroes = res;
+    }, err => {
+      console.log(err as string);
+      alert("Failed to fetch heroes");
+    })
   }
 }
